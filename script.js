283ee374f70d69ee87e59cfc9e31580daec6b13f
Weiss Bai <whitesgr03@gmail.com>
2022-09-22 17:51:35 +0800

    Refactoring Js
    The controller function performs role selection and name modification.

--- script.js
+++ script.js
@@ -1,206 +1,160 @@
 'use strict'
 
 const controller = (() => {
-
-    let playerName = {
-        'X': 'Player 1',
-        'O': 'Player 2',
-    }
-
-     // cache DOM
-    const startButton = document.querySelector('.startButton')
-    const pens = document.querySelectorAll('.editButton')
-    const cancelButtons = document.querySelectorAll('.cancelButton')
-    const confirmButtons = document.querySelectorAll('.confirmButton')
-    const score = document.querySelector('.score')
-
-    // bind events
-    startButton.addEventListener('click', startGame)
-    for (let pen of pens) {
-        pen.addEventListener('click', editName);
-    }
-    for (let Button of cancelButtons) {
-        Button.addEventListener('click', cancelEdit);
-    }
-    for (let Button of confirmButtons) {
-        Button.addEventListener('click', saveEdited);
-    }
-
-    function startGame() {
-        const gameBoard = document.querySelector('.gameBoard')
-        const parent = document.querySelector('.menu');
-
-        score.firstElementChild.textContent = `${playerName['X']} : 0`
-        score.lastElementChild.textContent = `${playerName['O']} : 0`
-
-        gameBoard.previousElementSibling.lastElementChild.textContent = playerName['X'];
-        gameBoard.nextElementSibling.lastElementChild.textContent = playerName['O'];
-        parent.classList.remove('active')
-    }
-
-    function editName() {
-        const character = this.closest('.avatar')
-        character.classList.add('editing');
-    }
-
-    function cancelEdit() {
-        const character = this.closest('.avatar')
-        character.classList.remove('editing');
-    }
-
-    function saveEdited() {
-        const character = this.closest('.avatar')
-        const input = this.closest('.edit').firstElementChild
-        const name = this.closest('.edit').previousElementSibling.firstElementChild
-
-        name.textContent = input.value
-        playerName[input.dataset.player] = input.value
-
-        character.classList.remove('editing');
-    }
-
-    function getName() {
-        return playerName;
-    }
-
-    return {
-        getName,
-    }
-})()
-
-
-const gameBoard = (() => {
-    let currentRound = 1;
-    let currentScore = {
-        'X': 0,
-        'O': 0,
-    }
-    let firstPlayer = true;
-    let winner = null;
-    let symbols = [];
-    const names = controller.getName()
-
-    for (let i = 0; i < 3; i++) {
-        symbols.push(Array.from({ length: 3 }))
-    }
-
+    const playerName = ['Player 1']
+    let fields = null;
+    let editNameButtons = null
+    
     // cache DOM
-    const cell = Array.from(document.querySelectorAll('.cell'));
-    const gameBoard = document.querySelector('.gameBoard');
-    const resetButton = document.querySelector('.resetButton');
-    const gameSettlement = document.querySelector('.settlementMessage')
-    const main = document.querySelector('main')
-    const score = document.querySelector('.score')
-    const players = document.querySelectorAll('main .avatar')
-    const restartButton = document.querySelector('.restart')
-    const parent = document.querySelector('.menu');
-
+    const characters = document.querySelector('.characters')
+    const firstPlayerName = document.querySelectorAll('.firstPlayerName')
+    const opponentButtons = document.querySelector('.opponentButtons')
+    const startGameButton = document.querySelector('.startGameButton')
+    const messageButton = document.querySelector('.messageButton')
 
     // bind events
-    gameBoard.addEventListener('click', addSymbols);
-    resetButton.addEventListener('click', resetGame);
-    restartButton.addEventListener('click', restartGame)
+    opponentButtons.addEventListener('click', chooseOpponent)
+    messageButton.addEventListener('click', reselectOpponent)
 
-    function addSymbols(e) {
-        if (
-            main.classList.contains('gameOver')||
-            !e.target.classList.contains('cell') ||
-            e.target.textContent
-        ) {
-            return
+    
+    function chooseOpponent(e) {
+        let characterTemplate = null
+        
+        const div = document.createElement('div')
+
+        playerName[1] = e.target.dataset.opponent;
+        
+        if (playerName[1] === 'player 2') {
+            characterTemplate = `
+                <div class="character player">
+                    <img src="./img/player.svg" alt="Player 2 avatar" class="avatar">
+                    <div>
+                        <p class="secondPlayerName"></p>
+                        <button type="button" class="editNameButton"></button>
+                    </div>
+                    <div class="field">
+                        <input class="nameField" type="text" value="" data-player="O" maxlength="8">
+                        <div class="fieldButtons">
+                            <button type="button" class="confirmButton"></button>
+                            <button type="button" class="cancelButton"></button>
+                        </div>
+                    </div>
+                </div>
+            `
+        } else {
+            characterTemplate = `
+                <div class="character robot">
+                    <img src="./img/robot.svg" alt="computer" class="avatar">
+                    <p>Com</p>
+                </div>
+            `
         }
 
-        const cellIndex = cell.findIndex(item => item === e.target)
+        div.innerHTML = characterTemplate
+        this.style.display = 'none'
+        characters.append(div)
+        init();
+    }
 
-        let line = null;
+    function reselectOpponent() {
+        opponentButtons.style.display = 'flex'
+        characters.lastElementChild.remove()
+        startGameButton.classList.add('hidden');
+        this.textContent = 'choose your opponent';
+
+        for (let i = 0; i < editNameButtons.length; i++) {
+            editNameButtons[i].style.display = 'none'
+        }
+
+        messageButton.classList.remove('reselect')
+
+        playerName[0] = 'Player 1'
+        renderPlayerName();
+    }
+
+    function init() {
+        fields = document.querySelectorAll('.nameField');
+
+        const cancelButtons = document.querySelectorAll('.cancelButton')
+        const confirmButtons = document.querySelectorAll('.confirmButton')
+        editNameButtons = document.querySelectorAll('.editNameButton')
+
+        for (let i = 0; i < editNameButtons.length; i++) {
+            editNameButtons[i].style.display = 'block'
+        }
+
+        startGameButton.classList.remove('hidden');
+        messageButton.classList.add('reselect')
+        messageButton.textContent = 're-select opponent';
+
+        bindEvents(editNameButtons, 'click', showInputField);
+        bindEvents(cancelButtons, 'click', cancelEdit);
+        bindEvents(confirmButtons, 'click', editPlayerName);
+        
+        renderPlayerName();
+    }
+    
+    function bindEvents(list, event, listener) {
+        for (let item of list) {
+            item.addEventListener(event, listener)
+        }
+    }
+
+
+    // render name
+    function renderPlayerName() {
+        for (let i = 0; i < firstPlayerName.length; i++) {
+            firstPlayerName[i].textContent = playerName[0]
+        }
+
+        for (let i = 0; i < fields.length; i++) {
+            fields[i].value = playerName[i]
+        }
+
+        renderOpponentName(playerName[1])
+    }
+
+    function renderOpponentName(name) {
+        if (!name) return 
+
+        const secondPlayerName = document.querySelectorAll('.secondPlayerName');
+
+        for (let i = 0; i < secondPlayerName.length; i++) {
+                secondPlayerName[i].textContent = name;
+            }
+    }
+
+
+    // edit
+    function showInputField() {
+        this.closest('.character.player').classList.add('editing')
+    }
+    
+    function cancelEdit() {
+        this.closest('.character.player').classList.remove('editing')
+    }
+
+    function editPlayerName(id, value) {
+        let name = null;
         let index = null;
 
-        if (cellIndex <= 2) {
-            line = 0;
-            index = cellIndex;
-        } else if (cellIndex >= 6){
-            line = 2;
-            index = cellIndex - 6
+        if (typeof value == 'string' && typeof id === 'number') {
+            name = value;
+            index = id - 1;
         } else {
-            line = 1;
-            index = cellIndex - 3
-        }
-        
-        let gameSymbol = null;
+            const target = this.closest('.field').firstElementChild;
 
-        if (firstPlayer) {
-            gameSymbol = 'X'
-        } else {
-            gameSymbol = 'O'
+            name = target.value;
+            index = Array.from(fields).findIndex(filed => filed === target);
+            cancelEdit.call(this);
         }
 
-        symbols[line][index] = gameSymbol;
-
-        for (let player of players) {
-            player.classList.toggle('mover');
-        }
-
-        render();
-        showWinner();
-
-        firstPlayer = !firstPlayer;        
+        playerName[index] = name;
+        renderPlayerName();
     }
 
-    function render() {
-        for (let i = 0; i < symbols.length; i++){
-            cell[i * 3 + 0].textContent = symbols[i][0]
-            cell[i * 3 + 1].textContent = symbols[i][1]
-            cell[i * 3 + 2].textContent = symbols[i][2]
-        }
-    }
+})()
 
-    function resetGame() {
-        const line = createLine()
-
-        if (line.every(symbol => typeof symbol === 'undefined')) {
-            return
-        }
-
-        if (winner === null) {
-            if (!confirm('Do you want to reset this round?')) {
-                return
-            }
-        }
-
-        symbols.length = 0;
-
-        for (let i = 0; i < 3; i++) {
-            symbols.push(Array.from({ length: 3 }))
-        }
-
-        render();
-        firstPlayer = true;
-        
-
-        if (winner !== null) {
-            winner = null;
-            main.classList.remove('gameOver')
-            gameSettlement.classList.remove('active');
-            players[0].classList.add('mover');
-        }
-    }
-
-    function showWinner() {
-
-        // check each row line
-        checkLine(symbols)
-
-        // check each column line
-        checkLine(createLine('column'));
-
-        // check each Slant Line
-        checkLine(createLine('slant'));
-
-        checkGameTie(createLine())
-
-        if (winner !== null) {
-            main.classList.add('gameOver');
-            gameSettlement.classList.add('active');
 
             for (let player of players) {
                 player.classList.remove('mover');
